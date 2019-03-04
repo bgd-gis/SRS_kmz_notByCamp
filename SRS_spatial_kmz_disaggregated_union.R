6c4b4e49e48b4f394a3a50e23192011a3f00552a
j-lapp <jarodlapp@gmail.com>
2019-03-04 16:08:00 +0600

    more scripts now

--- SRS_spatial_kmz_disaggregated_union.R
+++ SRS_spatial_kmz_disaggregated_union.R
@@ -27,67 +27,76 @@ library(dplyr)
 
 #IF YOUR SAMPLING FRAME IS DEFINED BY CAMP OR IF YOU NEED TO SPLIT KMZ FILES USE CAMP LATEST BOUNDARY SHPEFILE
 #DONT TRUST DATA SET TO GET CAMP BOUNDARIES- USE ACTUALY CAMP BOUNDARY FILE
-gdb2<-#"INPUT PATH TO GDB/FOLDER/GPKG CONTAINNG CAMP BOUNDARY"
+gdb<-"C:\\Users\\zacka\\Documents\\IMPACT\\REACH_Bangladesh\\01_general_gis\\03_shapefiles\\01_boundaries\\01_admin_boundaries"#"INPUT PATH TO GDB/FOLDER/GPKG CONTAINNG CAMP BOUNDARY"
   #LOAD SHELTER POINTS AND ADMIN BOUNDARIES
 # ogrListLayers(gdb1)
-ogrListLayers(gdb2) #HERE YOU CAN SEE THE NAMES OF THE GEOSPATIAL FILES IN THE GDB2 YOU DEFINED
+ogrListLayers(gdb) #HERE YOU CAN SEE THE NAMES OF THE GEOSPATIAL FILES IN THE GDB2 YOU DEFINED
 #LOAD IN CAMP BOUNDARIES
-cmp<-readOGR(dsn=gdb2, layer="Camp_Boundary_01102018")
+unions<-readOGR(dsn=gdb, layer="BGD_Teknaf_Ukhia_Unions")
 
 ##########***
-hh_centroid_gdb<-"C:/Users/zacka/Documents/IMPACT/REACH_Bangladesh/03_70DRF_UNICEF/04_Education/shelters_r3_centroid"
-ogrListLayers(hh_centroid_gdb)
-hh<-readOGR(dsn=hh_centroid_gdb, layer="shelters_r3_centroid")
+hh_centroid_gdb<-"C:/Users/zacka/Documents/IMPACT/REACH_Bangladesh/02_70DIY_ECHO/05_MSNA/01_data/01_raw_data/ISCG_HC_MSNA_Questionnaire_Final_V2_2018_Unclean_compiled_AllData.csv"
+# ogrListLayers(hh_centroid_gdb)
+# hh<-readOGR(dsn=hh_centroid_gdb, layer="shelters_r3_centroid")
 #SPATIAL TRANSFOR TO SAME PROJECTION
-hh2<-spTransform(hh,CRS=CRS(proj4string(cmp)))
+hh<-read.csv(hh_centroid_gdb,stringsAsFactors = FALSE, na.strings= c(NA,"<NA>","n/a",""," "))
+unique(hh$survey_consent)
+hh2<-hh[hh$survey_consent=="yes",]
+unique(hh2$survey_consent)
+hh2sp<-hh2
+coordinates(hh2sp)<-~X_gps_reading_longitude+X_gps_reading_latitude
+proj4string(hh2sp)<-proj4string(unions)
+
 #SPATIAL JOIN
-jd<-over(hh2, cmp)
-dfj<-data.frame(coordinates(hh2),jd)
+jd<-over(hh2sp, unions)
+dfj<-data.frame(coordinates(hh2sp),jd)
 
 #LOAD IN SAMPLING FRAME^****
-sample_frame_path<-"C:/Users/zacka/Documents/IMPACT/REACH_Bangladesh/03_70DRF_UNICEF/04_Education/"
-sf<-read.csv(paste0(sample_frame_path,"kmz_sample_request_template - sample_variation1(1).csv"), stringsAsFactors = FALSE, na.strings =c("n/a",""," ", NA, "NA","<NA>"))
-
-sf2<-sf[!is.na(sf$points_needed),]
-sf2$Camp_Name
-hh_ss<-merge(dfj, sf2, by.x="New_Camp_N", by.y="Camp_Name", all.x=TRUE, all.y=FALSE)
-hh_ss2<-hh_ss[!is.na(hh_ss$points_needed),]
-hh_ss2$New_Camp_N<-as.character(hh_ss2$New_Camp_N)
+# sample_frame_path<-"C:/Users/zacka/Documents/IMPACT/REACH_Bangladesh/03_70DRF_UNICEF/04_Education/"
+# sf<-read.csv(paste0(sample_frame_path,"kmz_sample_request_template - sample_variation1(1).csv"), stringsAsFactors = FALSE, na.strings =c("n/a",""," ", NA, "NA","<NA>"))
+# 
+# sf2<-sf[!is.na(sf$points_needed),]
+# sf2$Camp_Name
+# hh_ss<-merge(dfj, sf2, by.x="New_Camp_N", by.y="Camp_Name", all.x=TRUE, all.y=FALSE)
+# hh_ss2<-hh_ss[!is.na(hh_ss$points_needed),]
+# hh_ss2$New_Camp_N<-as.character(hh_ss2$New_Camp_N)
 
 ##########***
 
-list_hh_by_camp<-split(hh_ss2,hh_ss2$New_Camp_N)
+list_hh_by_union<-split(dfj,dfj$adm4_en)
+lapply(list_hh_by_union, nrow)
 sf$number_points_w_buffer
-length(list_hh_by_camp)
-setwd("C:/Users/zacka/Documents/IMPACT/REACH_Bangladesh/03_70DRF_UNICEF/04_Education/market_kmz/")
+length(list_hh_by_union)
+setwd( "C:\\Users\\zacka\\Dropbox\\REACH_BGD\\REACH\\Ongoing\\70DIY - ECHO\\MSNA\\GIS_HC_MSNA\\FGD_Points")
 #get random seed- but then store it for later under set
 seed_val<-sample(1:1000000,1)
-set.seed(385840)#385840
+set.seed(543149)#385840
 seed_list<-list()
 list_r1_samples<-list()
-for ( i in 1: length(list_hh_by_camp)){
+for ( i in 1: length(list_hh_by_union)){
   seed_val<-sample(1:1000000,1)
   #store seeds in a list so that you can repeat this later
   seed_list[[i]]<-seed_val
   set.seed(seed_val)
-  sr1<-sample(nrow(list_hh_by_camp[[i]]),list_hh_by_camp[[i]][1,"number_points_w_buffer"])
-  sr1_samps<-list_hh_by_camp[[i]][sr1,]
+  sr1<-sample(nrow(list_hh_by_union[[i]]),20)
+  sr1_samps<-list_hh_by_union[[i]][sr1,]
   list_r1_samples[[i]]<-sr1_samps
-  #get number of samples and camp name for name naming purposes
-  num_samples<-unique(sr1_samps[,"number_points_w_buffer"])
-  camp_name<-unique(sr1_samps[,"New_Camp_N"])
+  #get number of samples and union name for name naming purposes
+  num_samples<-20 #NORMALLY READ IN CSV AND EXTRACT NUMBER FROM ROW WITHIN BOUNDARY
+  union_name<-unique(sr1_samps[,"adm4_en"])
   sr1_sampsa<-sr1_samps
   sr1_sampsa$CID<-1:nrow(sr1_sampsa)
-  sr1_sampsb<-data.frame(sr1_sampsa$coords.x1, sr1_sampsa$coords.x2, sr1_sampsa$CID,sr1_sampsa$New_Camp_N)
-  cmp_name<-unique(sr1_sampsb$New_Camp_N)
-  colnames(sr1_sampsb)<-c("lon","lat","CID","camp_label")
+  sr1_sampsb<-data.frame(sr1_sampsa$X_gps_reading_longitude, sr1_sampsa$X_gps_reading_latitude, 
+                         sr1_sampsa$CID,sr1_sampsa$adm4_en)
+  un_name<-unique(sr1_sampsb$adm4_en)
+  colnames(sr1_sampsb)<-c("lon","lat","CID","union_label")
   sampsp<-sr1_sampsb
   coordinates(sampsp)<-~lon+lat
-  proj4string(sampsp)<-proj4string(cmp)
+  proj4string(sampsp)<-proj4string(unions)
   #write kml
-  kml(obj=sampsp, folder.name=paste0("second_draw_",camp_name,num_samples,"_market"), 
-      file.name=paste0("draw2_",camp_name,"_",num_samples,"_markets.kml"),
-      kmz=FALSE,altitude=0,plot.labpt=TRUE,labels=paste0(camp_name,sampsp$CID,"_market"),LabelScale=0.5)
+  kml(obj=sampsp, folder.name=paste0("FGD_draw1_",union_name,"_",num_samples), 
+      file.name=paste0("FGD_draw1_",union_name,"_",num_samples,".kml"),
+      kmz=FALSE,altitude=0,plot.labpt=TRUE,labels=paste0(sampsp$CID,union_name,"_fgd"),LabelScale=0.5)
   print(list_r1_samples[[i]] %>% nrow())
 }
 unique(hh_ss2$New_Camp_N)
@@ -95,11 +104,10 @@ unique(dfj$New_Camp_N)
 #take the seed list and save it into random seeds used
 dput(seed_list)
 seed.df<-do.call("rbind",seed_list)
-dput(seed.df)
+seed_list
 #save seed_list here so that you can repeat this and get same radomness
-rn_seeds_used<-c("35105", "948285", "858690", "545836", "109397", "413461", "715709", "22653", 
-                 "324296", "368849", "223820", "833881", "342267", "474441", "247924", "490651", 
-                 "312311", "794982", "368240")
+rn_seeds_used<-c("659152", "83130", "191899", "88587", "432505", "423963", "463387", 
+                 "347852", "826091")
 length(rn_seeds_used)
 
 
